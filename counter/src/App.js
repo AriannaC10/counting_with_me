b25bfdc755a39ea38492aee223178bbe8caef2e4
AriannaC10 <arianna.cignoni@gmail.com>
2023-10-08 18:04:49 +0200

    added chuck norris random joke

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const getChuckNorrisJokes = async () => {
+  console.log('here1')
+  const data = await fetch('https://api.chucknorris.io/jokes/random').then((res)=>res.json())
+  return data.value;
+}
 
 function App() {
   const [count, setCount] = useState(0); //saving numbers in a state
+  const [joke, setJoke] = useState(null)
 
   const increment = () => { //function to increment the counter
     setCount(count + 1);
+    getJoke();
   };
 
   const reset = () => { //function to reset the counter
     setCount(0);
   };
+
+  const getJoke = async () => {
+      const apiData = await getChuckNorrisJokes();
+      console.log('hereee', apiData)
+      setJoke(apiData)
+  }
+
+  useEffect(()=>{
+  
+    getJoke();
+
+  }, [])
+
 
   return (
     <div className="App">
@@ -24,6 +45,10 @@ function App() {
       <button className="increment-button" onClick={increment}>
         Increment
       </button>
+      <h3>Bonus joke: </h3>
+      <body>
+        {joke}
+      </body>
     </div>
   );
 }
