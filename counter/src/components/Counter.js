1719a6050cec37b85db134f1ae25969ef6f16de2
AriannaC10 <arianna.cignoni@gmail.com>
2023-10-10 11:31:17 +0200

    extend pure component class to reduce unnecessary rerendering

--- counter/src/components/Counter.js
+++ counter/src/components/Counter.js
@@ -1,67 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import "./Counter.css"
-import chuck from '../images/chuck-norris.png'
+import React, { PureComponent } from 'react';
+import './Counter.css';
+import chuck from '../images/chuck-norris.png';
 
 const getChuckNorrisJokes = async () => {
-  console.log('here1')
-  const data = await fetch('https://api.chucknorris.io/jokes/random').then((res)=>res.json())
+  console.log('here1');
+  const data = await fetch('https://api.chucknorris.io/jokes/random').then((res) => res.json());
   return data.value;
-}
+};
 
-function Counter() {
-  const [count, setCount] = useState(0); //saving numbers in a state
-  const [joke, setJoke] = useState(null)
+class Counter extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  const increment = () => { //function to increment the counter
-    setCount(count + 1);
-    getJoke();
-  };
-
-  const reset = () => { //function to reset the counter
-    setCount(0);
-  };
-
-  const getJoke = async () => {
-      const apiData = await getChuckNorrisJokes();
-      setJoke(apiData)
+    this.state = {
+      count: 0,
+      joke: null,
+    };
   }
 
-  useEffect(()=>{ //initialize the joke to display at first render
-  
-    getJoke();
+  increment = () => {
+    this.setState({ count: this.state.count + 1 }, this.getJoke);
+  };
 
-  }, [])
+  reset = () => {
+    this.setState({ count: 0 });
+  };
 
+  getJoke = async () => {
+    const apiData = await getChuckNorrisJokes();
+    this.setState({ joke: apiData });
+  };
 
-  return (
-    <div >
-      <h2 className='title'>We are serving number...</h2>
-      <div className="counter">
-        <label className={count === 0 ? 'zero' : ''}>Counter: {count}</label>
-      </div>
-      <button className="reset-button" onClick={reset}>
-        Reset
-      </button>
-      <button className="increment-button" onClick={increment}>
-        Increment
-      </button>
-      
-      <div className="joke">
-        <div className="joke_title">
-          <img src={chuck} alt="" style={{
-            height: "80px",
-            padding: "10px 0px 10px 0px",
-          }}
-          />
-          <subtitle>Bonus joke: </subtitle>
+  componentDidMount() {
+    this.getJoke();
+  }
+
+  render() {
+    const { count, joke } = this.state;
+
+    return (
+      <div>
+        <h2 className='title'>We are serving number...</h2>
+        <div className='counter'>
+          <label className={count === 0 ? 'zero' : ''}>Counter: {count}</label>
         </div>
+        <button className='reset-button' onClick={this.reset}>
+          Reset
+        </button>
+        <button className='increment-button' onClick={this.increment}>
+          Increment
+        </button>
 
-        <br/>
+        <div className='joke'>
+          <div className='joke_title'>
+            <img
+              src={chuck}
+              alt=''
+              style={{
+                height: '80px',
+                padding: '10px 0px 10px 0px',
+              }}
+            />
+            <subtitle>Bonus joke: </subtitle>
+          </div>
 
-        {joke}
+          <br />
+
+          {joke}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Counter;
